150472e6343779351f45e1ce33a362804db5a881
Steve Meredith <meredith.steve@gmail.com>
2014-04-20 21:43:25 -0700

    Parse command line to get file to process.
    Add boost.program_options via nuget to parse command line.

--- AverageColorConsoleApp/Main.cpp
+++ AverageColorConsoleApp/Main.cpp
@@ -3,23 +3,49 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <iomanip>
+#include <boost/program_options.hpp>
 #include <AverageColor.h>
+
+namespace po = boost::program_options;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+    po::options_description desc("Options");
+    desc.add_options()
+        ("help,h", "print help message")
+        ("input,i", po::wvalue<std::wstring>(), "input filename")
+        ;
+    
+    po::positional_options_description positionalOptions;     
+    positionalOptions.add("input", 1);
+
+    po::variables_map vm;
+
+    po::store(po::wcommand_line_parser(argc, argv).options(desc).positional(positionalOptions).run(), vm);
+    
+
+    if (vm.count("help") || !vm.count("input")) {
+        std::cout << "Find the average color of an image file." << std::endl << std::endl;
+        std::cout << "Usage: AverageColor [-h] [[-i] filename]" << std::endl << std::endl;
+        std::cout << desc << std::endl;
+        return 1;
+    }
+    
+    po::notify(vm);
     CoInitializeEx(nullptr, COINIT_MULTITHREADED);
 
     DWORD averageColor;
-
-    HRESULT hr = AverageColor(L"..\\TestFiles\\10000x10000_8080ff.jpg", averageColor);
+    HRESULT hr = AverageColor(vm["input"].as< std::wstring>().c_str(), averageColor);
 
     if (SUCCEEDED(hr))
     {
-        std::wcout << L"Average color: " << averageColor << std::endl;
+        std::cout << "0x" << std::hex << std::setw(6) << std::setfill('0') << averageColor << std::endl;
     }
     else
     {
-        std::wcout << L"Failed: " << hr << std::endl;
+        std::cout << "Failed: 0x" << std::hex << hr << std::endl;
     }
 
     CoUninitialize();
