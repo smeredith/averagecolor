357f65449d37431904a10e5b3155908f7cbb106d
Steve Meredith <meredith.steve@gmail.com>
2014-05-03 13:38:16 -0700

    Remove old files.

--- AverageColorLib/AverageColor_ParallelForEach.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-DWORD AverageColor_ParallelForEach(
-    const std::vector<BYTE>::const_iterator& begin,
-    const std::vector<BYTE>::const_iterator& end,
-    UINT width,
-    UINT height);
--- AverageColorLib/ColorCalculations.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <array>
-#include "RawBitmap.h"
-
-typedef std::array<ULONGLONG, 3> PixelColorSums;
-
-DWORD CalculateAverage(const PixelColorSums& totals, UINT pixelCount);
